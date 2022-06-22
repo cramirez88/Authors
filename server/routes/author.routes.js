139eedc4a7dfb4470efc678d324b0f765c6114af
Christian Ramirez <christian.r0602@gmail.com>
2022-06-22 10:58:31 -0500

    route to get all authors

--- server/routes/author.routes.js
+++ server/routes/author.routes.js
@@ -2,5 +2,6 @@ const AuthorController = require('../controllers/author.controller');
 module.exports = (app) => {
     // app.get('/api', PersonController.index);
     app.post('/api/author', AuthorController.createPerson);     
+    app.get('/api/author', AuthorController.getAllAuthors)
 }
 
